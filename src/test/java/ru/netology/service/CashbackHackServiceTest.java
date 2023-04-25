c30ed8b3ee717782999e34b577f9642964b3e62a
Netology QA <obgynkostina@gmail.com>
2023-04-25 18:27:20 +0300

    tests are ready

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,15 +7,46 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
         @Test
-        public void ShouldCountCashback() {
+        public void ShouldRemain1000IfAmountIs0() {
 
             CashbackHackService service = new CashbackHackService();
 
-            int actual = service.remain(2000);
+            int actual = service.remain(0);
             int expected = 1000;
             assertEquals(expected, actual);
 
         }
+    @Test
+    public void ShouldRemain1IfAmountIs999() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShouldRemain0IfAmountIs1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void ShouldRemain999IfAmountIs1001() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+        assertEquals(expected, actual);
+
+    }
     }
 
 
