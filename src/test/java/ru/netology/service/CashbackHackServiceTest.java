1520a5b5280870333033463e952aee9e358dfdf0
Netology QA <obgynkostina@gmail.com>
2023-04-25 17:49:35 +0300

    first test is created

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void ShouldCountCashback() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(2000);
+        int expected = 1000;
+        assertEquals(expected, actual);
+
+    }
+}
