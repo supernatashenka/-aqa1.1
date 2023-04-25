f37ba2110dcec729aab6ab475f6e2c6a2c2ca5c4
Netology QA <obgynkostina@gmail.com>
2023-04-25 18:33:07 +0300

    tests are ready,actual/expected

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,14 +7,45 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void ShouldCountCashback() {
+    public void ShouldRemain1000IfAmountIs0() {
 
         CashbackHackService service = new CashbackHackService();
 
-        int actual = service.remain(2000);
+        int actual = service.remain(0);
         int expected = 1000;
-        assertEquals( actual, expected);
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void ShouldRemain1IfAmountIs999() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual,expected);
 
     }
 
-}
+    @Test
+    public void ShouldRemain0IfAmountIs1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void ShouldRemain999IfAmountIs1001() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+        assertEquals(actual,expected);
+
+    }
+    }
+
