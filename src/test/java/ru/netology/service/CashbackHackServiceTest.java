818b31e1aec9bb0d3bc016209d716d661ef49b0b
Netology QA <obgynkostina@gmail.com>
2023-04-25 18:35:03 +0300

    tests are ready,actual/expected

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
 
             int actual = service.remain(0);
             int expected = 1000;
-            assertEquals(expected, actual);
+            assertEquals(actual, expected);
 
         }
     @Test
@@ -23,7 +23,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(999);
         int expected = 1;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
 
@@ -34,7 +34,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
     @Test
@@ -44,7 +44,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(1001);
         int expected = 999;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
     }
